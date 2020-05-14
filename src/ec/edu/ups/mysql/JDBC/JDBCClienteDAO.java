f368438f1a99a4e4789d5e4d2f3c68f83577a577
bguzman012 <46763361+bguzman012@users.noreply.github.com>
2020-05-13 22:56:36 -0500

    CROOD_Cliente

--- src/ec/edu/ups/mysql/JDBC/JDBCClienteDAO.java
+++ src/ec/edu/ups/mysql/JDBC/JDBCClienteDAO.java
@@ -1,5 +1,8 @@
 package ec.edu.ups.mysql.JDBC;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import ec.edu.ups.DAO.ClienteDAO;
@@ -10,37 +13,75 @@ public class JDBCClienteDAO extends JDBCGenericDAO<Cliente, String> implements C
 	@Override
 	public void createTable() {
 		// TODO Auto-generated method stub
-		
+
+
 	}
 
 	@Override
-	public void create(Cliente entity) {
+	public void create(Cliente cliente) {
 		// TODO Auto-generated method stub
+		conexionUno.update("INSERT clientes VALUES ('" + cliente.getCedula() + "', '" + cliente.getNombre() + "', '" + cliente.getApellido() + "', " 
+				+ cliente.getCredito() +")");
 		
+
 	}
 
 	@Override
 	public Cliente read(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		Cliente cli = null;
+		ResultSet rs = conexionUno.query("Select * from Clientes where CLI_CEDULA =" + id );
+		try {
+			if (rs != null && rs.next()) {
+
+				cli = new Cliente(rs.getString("CLI_CEDULA"), rs.getString("CLI_NOMBRE"), 
+						rs.getString("CLI_APELLIDO"), rs.getDouble("CLI_CREDITO"));
+
+			}
+		}catch(SQLException e){
+			System.out.println(">>>WARNING (JDBCUserDAO:read): " + e.getMessage());
+
+		}
+		return cli;
+
 	}
 
 	@Override
-	public void update(Cliente entity) {
+	public void update(Cliente cliente) {
 		// TODO Auto-generated method stub
-		
+		conexionUno.update("UPDATE Clientes SET CLI_NOMBRE = '" + cliente.getNombre() + "', CLI_APELLIDO = '" + cliente.getApellido() + 
+				"', CLI_CREDITO = " + cliente.getCredito() + " WHERE CLI_CEDULA = '" + cliente.getCedula() + "'");
+
+
 	}
 
 	@Override
-	public void delete(Cliente entity) {
+	public void delete(Cliente cliente) {
 		// TODO Auto-generated method stub
-		
+		conexionUno.update("DELETE FROM Clientes WHERE CLI_CEDULA = '" + cliente.getCedula() + "'");
+
 	}
 
 	@Override
 	public List<Cliente> find() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Cliente> list = new ArrayList<Cliente>();
+		ResultSet rs = conexionUno.query("SELECT * FROM CLIENTES");
+		try {
+			while (rs.next()) {
+				Cliente cli = new Cliente(rs.getString("CLI_CEDULA"), rs.getString("CLI_NOMBRE"), 
+						rs.getString("CLI_APELLIDO"), rs.getDouble("CLI_CREDITO"));
+				list.add(cli);
+			}
+
+		}catch (SQLException e) {
+			System.out.println(">>>WARNING (JDBCShoppingBasketDAO:find): " + e.getMessage());
+		}
+
+		return list;
 	}
+
+	public void recargarCredito() {
+		String cadena;
+	}
+
 
 }
