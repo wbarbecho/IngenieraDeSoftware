c7f1603be9098482fca677f030ad5a88d61b22c5
bguzman012 <46763361+bguzman012@users.noreply.github.com>
2020-05-14 23:47:14 -0500

    ModifyMain

--- src/Main.java
+++ src/Main.java
@@ -27,160 +27,179 @@ public class Main {
 		boolean salir = false;
 		int opcion; //Guardaremos la opcion del usuario
 
+		boolean seguir = true;
 
 		boolean user = false;
 		boolean correcto = false;
 		int sesion = 0;
 
-		System.out.println("1. Inciar Sesion \t 2. Registrar");
-		
-		sesion = sn.nextInt();
+		while(seguir) {
 
-		if (sesion == 1) {
+			System.out.println("1. Inciar Sesion \n2. Registrar \n3. Salir");
 
-			System.out.println("Iniciar Sesion");
 
-			while (!correcto) {
-				JDBCClienteDAO cli = new JDBCClienteDAO();
-				List<Cliente> clientes = cli.find();
-				Cliente clienteGlobal = null;
+			sesion = sn.nextInt();
+			
+			switch (sesion) {
+			case 1:
+				System.out.println("Iniciar Sesion");
 
-				System.out.println("Ingresar Correo:");
-				String correo = sn.nextLine();
+				while (!correcto) {
+					JDBCClienteDAO cli = new JDBCClienteDAO();
+					List<Cliente> clientes = cli.find();
+					Cliente clienteGlobal = null;
 
-				System.out.println("Ingresar contrasenia:");
-				String contrasenia = sn.nextLine();
-				System.out.println("Mijo");
-				for (Cliente cliente : clientes) {
-					clienteGlobal = cli.readByAddress(correo);
-					if (cliente.getCorreo().equals(correo) && cliente.getContrasenia().equals(contrasenia)) {
+					System.out.println("Ingresar Correo:");
+					String correo = sn.next();
 
-						correcto = true;
+					System.out.println("Ingresar contrasenia:");
+					String contrasenia = sn.next();
+					System.out.println("Mijo");
+					for (Cliente cliente : clientes) {
+						clienteGlobal = cli.readByAddress(correo);
+						if (cliente.getCorreo().equals(correo) && cliente.getContrasenia().equals(contrasenia)) {
 
-						System.out.println(clienteGlobal);
-						System.out.println("Bienvenido usuari@: \n \t" + clienteGlobal.getNombre() + " " + clienteGlobal.getApellido());
-						if (clienteGlobal.getRol().equals("User"))
-							user = true;
+							correcto = true;
+
+							System.out.println("Bienvenido usuari@: \n \t" + clienteGlobal.getNombre() + " " + clienteGlobal.getApellido());
+							if (clienteGlobal.getRol().equals("User"))
+								user = true;
+
+						}
+
 
 					}
-
-
+					if (!correcto)
+						System.out.println("Usuario invalido, ingrese nuevamente, por favor!");
 				}
-				if (!correcto)
-					System.out.println("Usuario invalido, ingrese nuevamente, por favor!");
-			}
-			while (!salir) {
-				if(user) {
-
-				}else {
-
-
-
-
-					System.out.println("1. Registrar Libro:");
-					System.out.println("2. Listar Libros");
-					System.out.println("3. Listar Clientes");
-					System.out.println("4. Salir");
-					try {
-
-						System.out.println("Escoge una de las opciones");
+				while (!salir) {
+					if(user) {
+						
+						System.out.println("1. Actualizar Datos Personales");
+						System.out.println("2. Listar Libros Disponibles");
+						System.out.println("3. Listar Compras realizadas");
+						
+						System.out.println("Escoge una de las opciones:");
 						opcion = sn.nextInt();
 
-						switch (opcion) {
-						case 1:
-							System.out.println("Tipo de libro: 1. Digital; 2. Impreso");
-							int tipo = sn.nextInt();
-							System.out.println("Registrar Libro");
-							System.out.print("Ingrese ISBN:");
-							String isbn = sn.next();
+					}else {
 
-							System.out.print("Ingrese Titulo:");
-							String titulo = sn.next();
-							System.out.print("Ingrese Autor:");
-							String autor = sn.next();
-							System.out.print("Ingrese Edicion:");
-							String edicion = sn.next();
-							System.out.print("Ingreso Precio: ");
-							double precio = sn.nextDouble();
-							if(tipo == 1) {
 
-								LibroDigital lib = new LibroDigital(isbn, titulo, autor, edicion, "null", precio, null, 2, 0.04);
+						System.out.println("1. Registrar Libro:");
+						System.out.println("2. Listar Libros");
+						System.out.println("3. Listar Clientes");
+						System.out.println("4. Salir");
+						try {
 
-								LibroDigitalDAO libroDigitalDAO = DAOGuia.getGuia().getLibroDigitalDAO();
-								libroDigitalDAO.create(lib); 
-							}else {
-								LibroImpreso lib = new LibroImpreso(isbn, titulo, autor, edicion, "null", precio, null, 3, 0.02, 20.00);
-								LibroImpresoDAO libroImpresoDAO = DAOGuia.getGuia().getLibroImpresoDAO();
-								libroImpresoDAO.create(lib);
-							}
+							System.out.println("Escoge una de las opciones");
+							opcion = sn.nextInt();
 
-							break;
-						case 2:
-							System.out.println("Listar Libros");
-							System.out.println("Escoger Tipo de libro: 1. Digital; 2. Impreso");
-							int tipo1 = sn.nextInt();
-							if(tipo1==1) {
-								LibroDigitalDAO libroDigitalDAO = DAOGuia.getGuia().getLibroDigitalDAO();
-								List<LibroDigital> libroDig = libroDigitalDAO.find();
-								for (LibroDigital libroDigital : libroDig) {
-									System.out.println("Libro: " + libroDigital);
+							switch (opcion) {
+							case 1:
+								System.out.println("Tipo de libro: 1. Digital; 2. Impreso");
+								int tipo = sn.nextInt();
+								System.out.println("Registrar Libro");
+								System.out.print("Ingrese ISBN:");
+								String isbn = sn.next();
+
+								System.out.print("Ingrese Titulo:");
+								String titulo = sn.next();
+								System.out.print("Ingrese Autor:");
+								String autor = sn.next();
+								System.out.print("Ingrese Edicion:");
+								String edicion = sn.next();
+								System.out.print("Ingreso Precio: ");
+								double precio = sn.nextDouble();
+								if(tipo == 1) {
+
+									LibroDigital lib = new LibroDigital(isbn, titulo, autor, edicion, "null", precio, null, 2, 0.04);
+
+									LibroDigitalDAO libroDigitalDAO = DAOGuia.getGuia().getLibroDigitalDAO();
+									libroDigitalDAO.create(lib); 
+								}else {
+									LibroImpreso lib = new LibroImpreso(isbn, titulo, autor, edicion, "null", precio, null, 3, 0.02, 20.00);
+									LibroImpresoDAO libroImpresoDAO = DAOGuia.getGuia().getLibroImpresoDAO();
+									libroImpresoDAO.create(lib);
 								}
-							}else {
-								LibroImpresoDAO libroImpresoDAO = DAOGuia.getGuia().getLibroImpresoDAO();
-								List<LibroImpreso> libroImpr = libroImpresoDAO.find();
-								for (LibroImpreso libroImpreso : libroImpr) {
-									System.out.println("Libro: " + libroImpreso );
-								}
-							}
 
-							break;
-						case 3:
-							System.out.println("Listar Clientes");
-							ClienteDAO clienteDAO = DAOGuia.getGuia().getClienteDAO();
-							List<Cliente> cliList = clienteDAO.find();
-							for (Cliente cliente : cliList) {
-								System.out.println(cliente);
+								break;
+							case 2:
+								System.out.println("Listar Libros");
+								System.out.println("Escoger Tipo de libro: 1. Digital; 2. Impreso");
+								int tipo1 = sn.nextInt();
+								if(tipo1==1) {
+									LibroDigitalDAO libroDigitalDAO = DAOGuia.getGuia().getLibroDigitalDAO();
+									List<LibroDigital> libroDig = libroDigitalDAO.find();
+									for (LibroDigital libroDigital : libroDig) {
+										System.out.println("Libro: " + libroDigital);
+									}
+								}else {
+									LibroImpresoDAO libroImpresoDAO = DAOGuia.getGuia().getLibroImpresoDAO();
+									List<LibroImpreso> libroImpr = libroImpresoDAO.find();
+									for (LibroImpreso libroImpreso : libroImpr) {
+										System.out.println("Libro: " + libroImpreso );
+									}
+								}
+
+								break;
+							case 3:
+								System.out.println("Listar Clientes");
+								ClienteDAO clienteDAO = DAOGuia.getGuia().getClienteDAO();
+								List<Cliente> cliList = clienteDAO.find();
+								for (Cliente cliente : cliList) {
+									System.out.println(cliente);
+								}
+								break;
+							case 4:
+								salir = true;
+								break;
+							default:
+								System.out.println("Solo números entre 1 y 4");
 							}
-							break;
-						case 4:
-							salir = true;
-							break;
-						default:
-							System.out.println("Solo números entre 1 y 4");
+						} catch (InputMismatchException e) {
+							System.out.println("Debes insertar un número");
+							sn.next();
 						}
-					} catch (InputMismatchException e) {
-						System.out.println("Debes insertar un número");
-						sn.next();
 					}
 				}
+
+				System.out.println("Gracias por su visita, vuelva pronto amig@!");
+
+				break;
+			
+			
+
+			case 2:
+				
+
+				System.out.println("Registrar Sesion");
+				System.out.print("Ingrese Cedula: ");
+				String cedula = sn.next();
+				System.out.print("Ingrese Nombre: ");
+				String nombre = sn.next();
+				System.out.print("Ingrese Apellido: ");
+				String apellido = sn.next();
+				System.out.print("Ingrese Credito: ");
+				double credito = sn.nextDouble();
+				System.out.println("Ingrese Correo: ");
+				String correo = sn.next();
+				System.out.println("Ingrese Contrasenia: ");
+				String password = sn.next();
+				Cliente cli = new Cliente(cedula, nombre, apellido, credito, correo, password, "User");
+				ClienteDAO clienteDAO = DAOGuia.getGuia().getClienteDAO();
+				clienteDAO.create(cli);
+				System.out.println("Gracias por unirse a nosotros!");
+
+
+				break;
+				
+			case 3:
+				
+				seguir = false;
+				
+			default:
+				break;
 			}
-
-			System.out.println("Gracias por su visita, vuelva pronto amig@!");
-
-		}else {
-			System.out.println("Registrar Sesion");
-			System.out.print("Ingrese Cedula: ");
-			String cedula = sn.next();
-			System.out.print("Ingrese Nombre: ");
-			String nombre = sn.next();
-			System.out.print("Ingrese Apellido: ");
-			String apellido = sn.next();
-			System.out.print("Ingrese Credito: ");
-			double credito = sn.nextDouble();
-			System.out.println("Ingrese Correo: ");
-			String correo = sn.next();
-			System.out.println("Ingrese Contrasenia: ");
-			String password = sn.next();
-			Cliente cli = new Cliente(cedula, nombre, apellido, credito, correo, password, "User");
-			ClienteDAO clienteDAO = DAOGuia.getGuia().getClienteDAO();
-			clienteDAO.create(cli);
-			System.out.println("Gracias por unirse a nosotros!");
-			
-			System.out.println("Bienvenid@ ");
-			System.out.print(nombre + " " + apellido);
-			
-			
-			
 		}
 	}
 }
